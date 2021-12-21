71da62861ef94809cf5a05b9e6734bb48599c543
Matt Szucs <mattszucs@Matts-MacBook-Pro-2.local>
2021-12-21 15:05:59 -0700

    Adding the ability to autogenerate foldernames based on the filename

--- Shapemap_ShellScript.sh
+++ Shapemap_ShellScript.sh
@@ -12,9 +12,9 @@ set -e # exit on first error (if any)
 # resolving (possibly nested) symlinks
 
 shapemapper \
---name "211215_1m7_top_2_trimmed" \
---out "211215_1m7_top_2_trimmed_OUT" \
---temp "211215_1m7_top_2_trimmed_TEMP" \
+--name "$0" \
+--out "$0_OUT" \
+--temp "$0_TEMP" \
 --target POWV_3UTR.fa \
 --verbose \
 --overwrite \
